e2635f76811b0b0783ac9cc33c6e0efa2d37593b
Jim McGee <jmcgee86@gmail.com>
2018-02-06 01:37:16 +0000

    error trapping of get one hotel request working with if else statements

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -33,6 +33,9 @@ module.exports.hotelsGetAll = function (req, res){
     
     var offset = 0;
     var count = 5;
+    var maxCount = 10;
+    
+    
     
     if (req.query && req.query.lat && req.query.lng){
         runGeoQuery(req, res);
@@ -47,15 +50,40 @@ module.exports.hotelsGetAll = function (req, res){
       count = parseInt(req.query.count, 10);
     };
 
+    if (isNaN(offset) || isNaN(count)){
+        res
+            .status(400)
+            .json({
+                "message" : "If supplied in querystring count and offset should be numbers"
+            });
+        return;
+    };
+    
+    if (count > maxCount){
+        res
+            .status(400)
+            .json({
+                "message": "Count limit of " + maxCount + " exceeded" 
+            });
+        return;
+    };
+
     Hotel
         .find()
         .skip(offset)
         .limit(count)
         .exec(function(err, hotels){
+            if (err){
+                console.log("Error finding hotels");
+                res
+                    .status(500)
+                    .json(err);
+            }else{
             console.log("Found hotels", hotels.length);
             res
                 .json(hotels);
-    })
+            }
+    });
 
 };
 
@@ -66,9 +94,22 @@ module.exports.hotelsGetOne = function (req, res){
     Hotel
     .findById(hotelId)
     .exec(function(err, doc){
+        if (err){
+                console.log("Error finding hotel");
+                res
+                    .status(500)
+                    .json(err);
+            }else if(!doc){
+                res
+                    .status(404)
+                    .json({
+                        "message": "Hotel ID not found"
+                    });
+            }else{
             res
                 .status(200)
                 .json(doc);
+            }    
         });
     
 };
