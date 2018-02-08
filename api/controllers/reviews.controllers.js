0662a326e78301d50a1c52d16ab75f0f9cf22a04
Jim McGee <jmcgee86@gmail.com>
2018-02-08 21:33:07 +0000

    update review added but not working properly

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -30,7 +30,6 @@ module.exports.reviewsGetAll = function(req,res){
             .json(doc.reviews);
         }
     });
- 
 };
 
 //GET single review for a hotel
@@ -44,34 +43,33 @@ module.exports.reviewsGetOne = function(req,res){
     .findById(hotelId)
     .select('reviews')//only returns reviews instead of all the hotel data
     .exec(function(err, hotel){
-        //var review = hotel.reviews.id(reviewId);
-
+        var response = {
+            status: 200,
+            message: {}
+        };
         if (err){
             console.log("error finding hotel")
-            res
-                .status(500)
-                .json(err)
+            response.status = 500;
+            response.message = err;
         }else if(!hotel){
-            console.log("hotel id not found", hotelId)
-            res
-                .status(404)
-                .json({
+            console.log("hotel id not found in database", hotelId)
+            response.status = 404;
+            response.message = {
                     "message": "hotel id of " + hotelId +  "notfound"
-                })
+                };
         }else{
-        var review = hotel.reviews.id(reviewId);
-        if (!review){
-            res
-                .status(404)
-                .json({
+        response.message = hotel.reviews.id(reviewId);
+        if (!response.message){
+            response.status = 404;
+            response.message ={
                     "message": "review ID of " + reviewId + " not found"
-                })
-        }else{
-        res
-            .status(200)
-            .json(review);
-        } 
+                };
+            }
         }
+        res
+            .status(response.status)
+            .json(response.message);
+         
     });
 };
 
@@ -119,11 +117,68 @@ module.exports.reviewsAddOne = function (req,res){
                     "message": "hotel id of " + hotelId + " not found"
                 });
         }if (doc){
-            _addReview(req,res, doc)
+            _addReview(req,res,doc)
         }
     });
 };
 
 module.exports.reviewsUpdateOne = function (req, res){
+    var hotelId = req.params.hotelId;
+    console.log("GET hotelId", hotelId);
+    var reviewId = req.params.reviewId;
+    console.log("GET reviewId " + reviewId + "for hotelId " + hotelId);
     
+    Hotel
+        .findById(hotelId)
+        .select('reviews')//only returns reviews instead of all the hotel data
+        .exec(function(err, hotel){
+            var response = {
+                status: 200,
+                message: {}
+            };
+            if (err){
+                console.log("error finding hotel")
+                response.status = 500;
+                response.message = err;
+            }else if(!hotel){
+                console.log("hotel id not found in database", hotelId)
+                response.status = 404;
+                response.message = {
+                        "message": "hotel id of " + hotelId +  "notfound"
+                    };
+            }else{
+            response.message = hotel.reviews.id(reviewId);
+            if (!response.message){
+                response.status = 404;
+                response.message ={
+                        "message": "review ID of " + reviewId + " not found"
+                    };
+                }
+            }if(response.status != 200){
+                    res
+                        .status(response.status)
+                        .json(response.message);
+                }else{
+                    console.log("trying to add the updated data");
+                    response.message.name = req.body.name;
+                    response.message.rating = parseInt(req.body.rating,10);
+                    response.review = req.body.review;
+                
+                 hotel.save(function(err, reviewUpdated){
+                    if(err){
+                        res
+                            .status(500)
+                            .json(err);
+                    }else{
+                        console.log('tried to update', reviewUpdated);
+                        res
+                            .status(204)
+                            .json(reviewUpdated);
+                    }
+                });
+                };
+             
+        });
+  
+        
 };
